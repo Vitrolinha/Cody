2990375a31dd89fffaf78f4ba1334f55f8561d8f
DMVMarcio <playdznvlt@gmail.com>
2019-02-17 22:45:55 -0300

    tempmute

--- events/message.js
+++ events/message.js
@@ -29,7 +29,7 @@ module.exports = async function (message) {
                     escapeValue: false
                 },
                 returnEmptyString: false
-            }, (e, f) => {
+            }, async (e, f) => {
                 if (f) {
                     if(!usuario.banned.get('ban')) {
                         if (message.content.startsWith(servidor.prefix)) {
