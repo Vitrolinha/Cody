1f4bf688b7e524e01b9c41be447078f265018e70
DMVMarcio <playdznvlt@gmail.com>
2019-03-06 11:40:14 -0300

    discrim

--- commands/discrim.js
+++ commands/discrim.js
@@ -1,4 +1,5 @@
 const { command } = require('../utils')
+const inWindow = []
 
 module.exports = class Lock extends command {
     constructor (name, client) {
@@ -6,11 +7,12 @@ module.exports = class Lock extends command {
         this.aliases = ['discriminator']
     }
     async run ({message, args}) {
+        if(inWindow.includes(message.author.id)) return message.channel.send(t('comandos:discrim.inWindow'))
         if(!args[0]) return message.channel.send(t('comandos:discrim.noArgs'))
         let discrim = args[0].replace('#')
         if(discrim.length > 4) return message.channel.send(t('comandos:discrim.discrimLength'))
         if(isNaN(discrim)) return message.channel.send(t('comandos:discrim.isNaN'))
-        let founds = this.client.users.filter(user => user.discriminator == discrim)
+        let founds = this.client.users.filter(user => user.discriminator === discrim)
         if(founds.size === 0) return message.channel.send(t('comandos:discrim.notFound', { discrim: discrim }))
         let totalPages = parseInt(founds.size/25)
         let pagina = 1
@@ -26,6 +28,48 @@ module.exports = class Lock extends command {
             if(totalPages === 0) return;
             await msg.react('⬅')
             await msg.react('➡')
+            await msg.react('❌')
+            var force = false
+            const anterior = msg.createReactionCollector((r, u) => r.emoji.name === "⬅" && u.id === message.author.id, { time: 60000 });
+            const proxima = msg.createReactionCollector((r, u) => r.emoji.name === "➡" && u.id === message.author.id, { time: 60000 });
+            const finalizar = msg.createReactionCollector((r, u) => r.emoji.name === "❌" && u.id === message.author.id, { time: 60000 });
+            inWindow.push(message.author.id)
+            anterior.on('collect', async r => {
+                r.remove(r.users.last().id).catch(e => {})
+                if(pagina === 1) {
+                    pagina = totalPages + 1
+                    msg.edit(await genEmbed({page: pagina}))
+                } else {
+                    pagina -= 1
+                    msg.edit(await genEmbed({page: pagina}))
+                }
+            })
+            proxima.on('collect', async r => {
+                r.remove(r.users.last().id).catch(e => {})
+                if(pagina === (totalPages + 1)) {
+                    pagina = 1
+                    msg.edit(await genEmbed({page: pagina}))
+                } else {
+                    pagina += 1
+                    msg.edit(await genEmbed({page: pagina}))
+                }
+            })
+            finalizar.on('collect', async r => {
+                force = true
+                msg.delete().catch(e => {})
+                message.delete().catch(e => {})
+                anterior.emit('end')
+                proxima.emit('end')
+                finalizar.emit('end')
+            })
+            finalizar.on('end', async r => {
+                inWindow.splice(inWindow.indexOf(message.author.id), 1)
+                msg.delete().catch(e => {})
+                message.delete().catch(e => {})
+                if(force) return;
+                aprovar.emit('end')
+                reprovar.emit('end')
+            })
         })
     }
 }
