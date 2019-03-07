049c861b2b8c36f790587108ab5f9591dc36c8bb
DMVMarcio <playdznvlt@gmail.com>
2019-03-07 19:52:14 -0300

    sugest sistem

--- commands/config.js
+++ commands/config.js
@@ -6,7 +6,7 @@ module.exports = class Config extends command {
         this.aliases = ['configurar']
     }
     async run ({message, args, prefix, usuario, servidor}, t) {
-        if (!(await this.client.verPerm(['MANAGE_GUILD', 'owner', 'subowner', 'operator'], message.member, usuario))) return message.channel.send(t('comandos:config.noPermission'));
+        if(!(await this.client.verPerm(['MANAGE_GUILD', 'owner', 'subowner', 'operator'], message.member, usuario))) return message.channel.send(t('comandos:config.noPermission'));
         let configs = ['prefix', 'vipmessages', 'sugest']
         let embed = new this.client.Discord.RichEmbed()
             .setTitle(t('comandos:config.howToUse'))
@@ -14,22 +14,23 @@ module.exports = class Config extends command {
             .setTimestamp(new Date())
             .setFooter(message.author.username, message.author.displayAvatarURL)
             .setColor(2631906)
-        if (!args[0] && !configs.includes(args[0].toLowerCase())) return message.channel.send(embed);
+        if(!args[0]) return message.channel.send(embed);
+        if(!configs.includes(args[0].toLowerCase())) message.channel.send(embed)
         let config = args[0].toLowerCase()
-        if (config === 'prefix') {
-            if (!args[1]) return message.channel.send(t('comandos:config.prefix.noArgs'))
+        if(config === 'prefix') {
+            if(!args[1]) return message.channel.send(t('comandos:config.prefix.noArgs'))
             let newPrefix = args[1]
-            if (newPrefix.length > 4) return message.channel.send(t('comandos:config.prefix.bigPrefix'))
+            if(newPrefix.length > 4) return message.channel.send(t('comandos:config.prefix.bigPrefix'))
             servidor.prefix = newPrefix
             servidor.save()
             message.channel.send(t('comandos:config.prefix.defined', { prefix: newPrefix }))
-        } else if (config === 'vipmessages') {
+        } else if(config === 'vipmessages') {
             let oVip = servidor.config.get('vipMessages') ? false : true
             servidor.config.set('vipMessages', oVip)
             servidor.save()
             let msg = oVip ? t('comandos:config.vipMessages.defined') : t('comandos:config.vipMessages.removed')
             message.channel.send(msg)
-        } else if (config === 'sugest') {
+        } else if(config === 'sugest') {
             var actions = ['on', 'off']
             let sugest = new this.client.Discord.RichEmbed()
                 .setTitle(t('comandos:config.sugest.howToUse'))
@@ -37,7 +38,8 @@ module.exports = class Config extends command {
                 .setTimestamp(new Date())
                 .setFooter(message.author.username, message.author.displayAvatarURL)
                 .setColor(2631906)
-            if(!args[1] && !actions.includes(args[1].toLowerCase())) return message.channel.send(sugest);
+            if(!args[1]) return message.channel.send(sugest);
+            if(!actions.includes(args[1].toLowerCase())) return message.channel.send(sugest);
             var action = args[1].toLowerCase()
             message.channel.send(`${action}`)
         }
