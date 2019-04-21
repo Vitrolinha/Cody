27d6c6d1790f1c513a8559f20795f3883792d1f2
Marcio Henrique <zmarciogod@gmail.com>
2019-04-21 01:03:07 -0300

    codes

--- commands/codes.js
+++ commands/codes.js
@@ -5,14 +5,24 @@ module.exports = class Decode extends command {
         super (name, client)
         this.aliases = ['code']
     }
-    async run ({message, usuario}, t) {
-        let userDB = message.mentions.users.first() ? this.client.database.Users.findOne({'_id': message.mentions.users.first().id}) ? this.client.database.Users.findOne({'_id': message.mentions.users.first().id}) : usuario : usuario
-        let embed = new this.client.Discord.RichEmbed()
-            .setTitle(`${this.client.users.get(userDB._id).username}:`)
-            .setDescription(t('comandos:codes.desc', { codes: Number(userDB.economy.get('codes')).toLocaleString(), decoders: Number(userDB.economy.get('decoders')).toLocaleString() }))
-            .setTimestamp(new Date())
-            .setFooter(message.author.username, message.author.displayAvatarURL)
-            .setColor(5289)
-        message.channel.send(embed)
+    async run ({message}, t) {
+        let user = message.mentions.users.first() ? message.mentions.users.first() : message.author
+        let userDB = await this.client.Users.findOne({'_id': user.id})
+        if(userDB) {
+            let embed = new this.client.Discord.RichEmbed()
+                .setTitle(`${user.username}:`)
+                .setDescription(t('comandos:codes.desc', { codes: Number(userDB.economy.get('codes')).toLocaleString(), decoders: Number(userDB.economy.get('decoders')).toLocaleString() }))
+                .setTimestamp(new Date())
+                .setFooter(message.author.username, message.author.displayAvatarURL)
+                .setColor(5289)
+            message.channel.send(embed)
+        } else {
+            message.channel.send(t('comandos:codes.noUserDB'))
+            this.newDocDB({
+                id: user.id,
+                type: 1,
+                content: user
+            })
+        }
     }
 }
