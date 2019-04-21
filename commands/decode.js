dc77ec357045a207949d2d419db9e76868927f6b
Marcio Henrique <zmarciogod@gmail.com>
2019-04-21 01:23:48 -0300

    codes

--- commands/decode.js
+++ commands/decode.js
@@ -14,8 +14,9 @@ module.exports = class Decode extends command {
         if(parseInt(((Date.now() - usuario.economy.get('lastDecode'))/1800000)) === 0) return message.channel.send(t('comandos:decode.nothingToCollect', { member: message.member, codes: (usuario.economy.get('decoders') * 1000), time: minutes }));
         let codes = parseInt(usuario.economy.get('lastDecode')) === 0 ? usuario.economy.get('decoders') * 1000 : (parseInt((Date.now() - usuario.economy.get('lastDecode'))/1800000) * (usuario.economy.get('decoders') * 1000))
         let bonus = usuario.vip ? codes/2 : 0
+        let restante = (Date.now() - usuario.economy.get('lastDecode')) - ((codes/1000) * 1800000)
         usuario.economy.set('codes', (usuario.economy.get('codes') + codes + bonus))
-        usuario.economy.set('lastDecode', (Date.now()).toString())
+        usuario.economy.set('lastDecode', (Date.now() - restante).toString())
         usuario.save()
         let embed = new this.client.Discord.RichEmbed()
             .setTitle(t('comandos:decode.decoded'))
