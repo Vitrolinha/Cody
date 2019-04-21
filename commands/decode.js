89812590bfb02f69d85de0bb3477a2717154a4fa
Marcio Henrique <zmarciogod@gmail.com>
2019-04-20 21:22:53 -0300

    decode

--- commands/decode.js
+++ commands/decode.js
@@ -5,13 +5,13 @@ module.exports = class Decode extends command {
         super (name, client)
         this.aliases = ['decoder' ,'decodificar']
     }
-    run ({message, usuario}, t) {
+    async run ({message, usuario}, t) {
         let minutes = ((1800000 - (Date.now() - usuario.economy.get('lastDecode')))/1000)/60
         if(((Date.now() - usuario.economy.get('lastDecode'))/1800000) === 0) return message.channel.send(t('comandos:decode.nothingToCollect', { member: message.member, codes: (usuario.economy.get('decoders') * 1000), time: minutes }));
         let codes = usuario.economy.get('decoders') * 1000
-        usuario.economy.set('codes', codes)
-        usuario.economy.set('lastDecode', (Date.now()).toString())
-        usuario.save()
+        await usuario.economy.set('codes', codes)
+        await usuario.economy.set('lastDecode', (Date.now()).toString())
+        await usuario.save()
         let embed = new this.client.Discord.RichEmbed()
             .setTitle(t('comandos:decode.decoded'))
             .setDescription(t('comandos:decode.codes', { codes: codes }))
