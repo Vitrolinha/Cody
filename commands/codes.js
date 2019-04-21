b83039b6fef94bfea5dc4119f6b47d6ba61fa7bc
Marcio Henrique <zmarciogod@gmail.com>
2019-04-21 00:55:34 -0300

    decode

--- commands/codes.js
+++ commands/codes.js
@@ -6,7 +6,7 @@ module.exports = class Decode extends command {
         this.aliases = ['code']
     }
     async run ({message, usuario}, t) {
-        let userDB = message.mentions.users.first() ? this.client.Users.findOne({'_id': message.mentions.users.first().id}) ? this.client.Users.findOne({'_id': message.mentions.users.first().id}) : usuario : usuario
+        let userDB = message.mentions.users.first() ? this.client.database.Users.findOne({'_id': message.mentions.users.first().id}) ? this.client.database:Users.findOne({'_id': message.mentions.users.first().id}) : usuario : usuario
         let embed = new this.client.Discord.RichEmbed()
             .setTitle(`${this.client.users.get(userDB._id).username}:`)
             .setDescription(t('comandos:codes.desc', { codes: Number(userDB.economy.get('codes')).toLocaleString(), decoders: Number(userDB.economy.get('decoders')).toLocaleString() }))
