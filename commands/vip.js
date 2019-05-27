060e7126178aed82d51d5dc909a31fcf32d3a662
Marcio Henrique <zmarciogod@gmail.com>
2019-05-27 18:21:51 -0300

    capacitors in code command

---
+++ commands/vip.js
@@ -0,0 +1,11 @@
+const { command } = require('../utils')
+
+module.exports = class TempMute extends command {
+    constructor (name, client) {
+        super (name, client)
+        this.aliases = ['vote', 'votes', 'votar', 'voto']
+    }
+    async run ({message, args, usuario, servidor}) {
+        
+    }
+}
