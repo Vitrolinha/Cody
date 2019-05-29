ef84eac4e52ca09b2c9277195e1fcffb6114f464
Marcio Henrique <zmarciogod@gmail.com>
2019-05-29 19:39:19 -0300

    teste

--- Cody.js
+++ Cody.js
@@ -1,4 +1,4 @@
-const { Client, Collection } = require('discord.js')
+const { Client, Collection } = require('discord.js') // testes
 const { readdirSync, statSync } = require('fs')
 module.exports = class Cody extends Client {
     constructor (options = {}) {
