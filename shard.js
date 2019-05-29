6b6118bff734023979b755ee85670a646382a945
Marcio Henrique <zmarciogod@gmail.com>
2019-05-29 15:39:44 -0300

    bot

--- shard.js
+++ shard.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (request, response) => {
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT);
+
 const { ShardingManager } = require('discord.js')
 const manager = new ShardingManager(`./index.js`, { totalShards: 2 })
 
