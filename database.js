bde4bbff1ef3d419c924af2cd3c85914540d545e
Marcio Henrique <zmarciogod@gmail.com>
2019-05-31 13:48:54 -0300

    vip

--- database.js
+++ database.js
@@ -29,7 +29,7 @@ const User = new Schema({
   },
   vip: {
     type: Map,
-    default: { on: false, time: '0000000000000', votePoints: 0, warned: true, warns: false, verify: { day: 0, given: false, dbl: 0, bpd: 0, mbl: false } }
+    default: { on: false, time: '0000000000000', votePoints: 0, warned: true, warns: true, verify: { day: 0, given: false, dbl: 0, bpd: 0, mbl: false } }
   }
 })
 
--- shard.js
+++ shard.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (request, response) => {
     response.sendStatus(200);
   });
-  app.listen(process.env.PORT);
+app.listen(process.env.PORT);
 
 const { ShardingManager } = require('discord.js')
 const manager = new ShardingManager(`./index.js`, { totalShards: 2 })
