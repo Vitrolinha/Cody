ba13947f8986c6382dd9c9356b58b0c51882e539
Marcio Henrique <zmarciogod@gmail.com>
2019-06-21 10:12:56 -0300

    processor

--- commands/Economy/decode.js
+++ commands/Economy/decode.js
@@ -13,12 +13,12 @@ module.exports = class extends command {
         let coldown = usuario.altBought.includes('processor') ? 900000 : 1800000
         let minutes = parseInt(((coldown - (Date.now() - usuario.economy.get('lastDecode')))/1000)/60)
         if(parseInt(((Date.now() - usuario.economy.get('lastDecode'))/coldown)) === 0) return message.channel.send(t('comandos:decode.nothingToCollect', { member: message.member, codes: Number(usuario.economy.get('decoders') * 1000).toLocaleString(), time: minutes }));
-        let codes = parseInt(usuario.economy.get('lastDecode')) === 0 ? usuario.economy.get('decoders') * 1000 : (parseInt((Date.now() - usuario.economy.get('lastDecode'))/1800000) * (usuario.economy.get('decoders') * 1000))
+        let codes = parseInt(usuario.economy.get('lastDecode')) === 0 ? usuario.economy.get('decoders') * 1000 : (parseInt((Date.now() - usuario.economy.get('lastDecode'))/coldown) * (usuario.economy.get('decoders') * 1000))
         if(codes >= (usuario.economy.get('decoders') * 25000)) {
             codes = usuario.economy.get('decoders') * 25000
         }
         let bonus = usuario.vip.get('on') ? codes/2 : 0,
-            restante = parseInt(usuario.economy.get('lastDecode')) === Date.now() ? 0 : parseInt(parseInt((Date.now() - usuario.economy.get('lastDecode'))/1800000)*1800000);
+            restante = parseInt(usuario.economy.get('lastDecode')) === Date.now() ? 0 : parseInt(parseInt((Date.now() - usuario.economy.get('lastDecode'))/coldown)*coldown);
         usuario.economy.set('codes', (usuario.economy.get('codes') + codes + bonus))
         usuario.economy.set('lastDecode', (parseInt(usuario.economy.get('lastDecode')) + restante).toString())
         usuario.economy.set('warned', false)
